dd963f91279119d8c101207bbf384747057599d4
BaraaAldomani <baraadom9@gmail.com>
2023-12-01 18:42:43 +0300

    disable item from common section in settings

--- storage/framework/views/283456a967d617b3f0ba05438ab381aa.php
+++
@@ -1,2 +0,0 @@
-<div <?php echo e($attributes->merge(['class' => 'loading-spinner'])); ?>></div>
-<?php /**PATH /home/baraa/Desktop/main/platform/core/base/resources/views/components/loading.blade.php ENDPATH**/ ?>
--- storage/framework/views/a5ea575c571bbd370c577eaa445e3f7c.php
+++
@@ -1,5 +0,0 @@
-<div <?php echo e($attributes->merge(['class' => 'card-body'])); ?>>
-    <?php echo e($slot); ?>
-
-</div>
-<?php /**PATH /home/baraa/Desktop/main/platform/core/base/resources/views/components/card/body/index.blade.php ENDPATH**/ ?>
